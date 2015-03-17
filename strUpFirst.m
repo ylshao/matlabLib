c73e96a83b99de062836dd197fd7633db459bffe
ylshao <ylshao@umich.edu>
2015-03-17 00:53:47 -0400

    Mar16

--- strUpFirst.m
+++ strUpFirst.m
@@ -1,4 +1,6 @@
-function upFirstStr = strUpFirst(str)
+function strUpFirst(str)
     lowerStr = lower(str);
     upFirstStr = regexprep(lowerStr,'(\<[a-z])','${upper($1)}');
+    clipboard('copy', upFirstStr)
+    fprintf('%s\n',upFirstStr)
 end
